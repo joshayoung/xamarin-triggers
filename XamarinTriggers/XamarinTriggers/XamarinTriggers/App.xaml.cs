05d51c55871432f3e9b38cb9b1db05740e37d484
Josh Young <18503267+joshayoung@users.noreply.github.com>
2021-09-08 07:13:24 -0500

    Move 'Property Triggers' to its own page

--- XamarinTriggers/XamarinTriggers/XamarinTriggers/App.xaml.cs
+++ XamarinTriggers/XamarinTriggers/XamarinTriggers/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -11,23 +10,11 @@ namespace XamarinTriggers
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
+        protected override void OnStart() { }
+        protected override void OnSleep() { }
+        protected override void OnResume() { }
     }
 }
--- XamarinTriggers/XamarinTriggers/XamarinTriggers/MainPage.xaml.cs
+++ XamarinTriggers/XamarinTriggers/XamarinTriggers/MainPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinTriggers.Pages;
 
 namespace XamarinTriggers
 {
@@ -12,6 +9,11 @@ namespace XamarinTriggers
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PropertyTriggers(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PropertyTriggerPage());
         }
     }
 }
